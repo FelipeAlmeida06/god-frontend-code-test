c9f92d579d2e9fdfd3a8af42c3a420b731c4fd4a
Felipe Antônio de O. Almeida <oliveira.lipe.2006@gmail.com>
2025-08-16 13:04:55 -0300

    configurando axios - primeira parte

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,5 +1,17 @@
+// https://vcc-ui.vercel.app/docs/setup
+// Setup Volvo: npm install vcc-ui --save
+// npm install axios
+
 import React from "react";
+import { Block, Button } from "vcc-ui";
+import { useCars } from "../hooks/useCars";
 
 export const HelloWorld: React.FC = () => {
-  return <h1>Hello World!</h1>;
+  const {} = useCars();
+
+  return (
+    <Block extend={{padding: 20}}>
+      <Button>Click me!</Button>
+    </Block>
+  )
 };
