29a27e9f2fd3e9b10ab2a177ad76e19ba7ac6c63
Felipe Antônio de O. Almeida <oliveira.lipe.2006@gmail.com>
2025-08-17 15:44:42 -0300

    navegação mobile entre os cards

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -2,18 +2,20 @@
 // Setup Volvo: npm install vcc-ui --save
 // instalar: npm install axios
 
-import React from "react";
+import React, { useState } from "react";
 import { Block, Button, Flex, Text } from "vcc-ui";
 import { useCars } from "../hooks/useCars";
 import { CarCard } from "./CarCard";
 import { Spacer } from "./Spacer";
 
 import PaginationDesktop from "./PaginationDesktop";
+import PaginationMobile from "./PaginationMobile";
 
 import styles from "../../public/css/home.module.css"   // estilos
 
 export const HelloWorld: React.FC = () => {
   const {cars} = useCars();
+  const [activeIndex, setActiveIndex] = useState(-1);       // botoes pequeno (dots) para navegação mobile -> nenhum dot selecionado no início
 
   // Funções de navegação entre os cards:
   // onClickLeft volta um card para trás
@@ -54,6 +56,21 @@ export const HelloWorld: React.FC = () => {
     }
   }
 
+  const onClickMobile = (index: number) => {
+    setActiveIndex(index);
+
+    const cardList = document.getElementById("card-list");
+    if (!cardList) return;
+
+    const card = cardList.firstElementChild as HTMLElement;
+    const cardSize = card?.clientWidth ?? 0;
+
+    cardList.scrollTo({
+      left: cardSize * index,
+      behavior: "smooth"
+    });
+  }
+
   return (
     <div className={styles.homeWrapper}>
       <Text variant="cook">Todos os modelos Recharge</Text>
@@ -66,6 +83,10 @@ export const HelloWorld: React.FC = () => {
       </div>
 
       <PaginationDesktop onClickLeft={onClickLeft} onClickRight={onClickRight}/>
+
+      <PaginationMobile onClick={onClickMobile} total={cars.length} activeIndex={activeIndex} />
     </div>
   )
 };
+
+// navegação mobile entre os cards
--- src/components/PaginationMobile.tsx
+++ src/components/PaginationMobile.tsx
@@ -1,12 +1,21 @@
-interface PaginationMobileProps {
+import styles from "../../public/css/components/paginationMobile.module.css"
 
+interface PaginationMobileProps {
+    total: number,
+    activeIndex: number,
+    onClick: (i: number) => void;     // retorna void (vazio)
 }
 
-export default function PaginationMobile(props: PaginationMobileProps) {
+export default function PaginationMobile({total, onClick, activeIndex}: PaginationMobileProps) {
     return (
-        <div className="">
-            <button></button>
-            <button></button>
+        <div className={styles.btnWrapper}>
+            {Array.from({length: total}).map((element, i) => (
+                <button 
+                    className={`${styles.btn} ${i === activeIndex ? styles.active : ""}`}
+                    key={i} 
+                    onClick={() => onClick(i)}>
+                </button>
+            ))}
         </div>
     )
 }
